aedad6eac94b0841669de54b6d42589edf33652b
berglindb19 <berglindb19@ru.is>
2020-09-03 14:36:00 +0000

    first prufa

---
+++ Assignment5.py
@@ -0,0 +1,9 @@
+num_int = int(input("Input a number: "))    # Do not change this line
+
+while num_int >= 0:     
+    listi=num_int.split()
+    print(listi)
+    num_int = int(input("Input a number: "))
+
+
+# print("The maximum is", max_int)  
