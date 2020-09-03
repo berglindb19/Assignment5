684aa44cdab19c6300172e58e9c4689529fc4291
berglindb19 <berglindb19@ru.is>
2020-09-03 15:16:55 +0000

    Algorithm written for max_int

--- max_int.py
+++ max_int.py
@@ -1,3 +1,13 @@
+# Given a positive integer from the user until the input becomes a negative integral
+# First you need to ask the user for a positive integer
+# To be able to put all numbers together to a list you need to make an emtpy list
+# Next, make a while loop that asks user for a number while the input is not negative
+# Every positive integer that the user chooses needs to be added to the list
+# to find the maximum number in the list we can use the "max" command that will find the larges number
+
+
+
+
 num_int = int(input("Input a number: "))    # Do not change this line
 
 listi = []
@@ -5,7 +15,6 @@ listi = []
 while num_int >= 0:
     listi.append(num_int)     
     num_int = int(input("Input a number: "))
+max_int=max(listi)
+print("The maximum is", max_int)
 
-print("The maximum is", max(listi))
-
-# print("The maximum is", max_int)  
