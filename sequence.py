a5f2d2e59900f468a3617e081b678d5cc4aa0aac
berglindb19 <berglindb19@ru.is>
2020-09-03 15:02:31 +0000

    Code written for sequence

--- sequence.py
+++ sequence.py
@@ -9,3 +9,18 @@
 # for all n >= 3 the algorithm calculated the sum of the last 3 numbers in the sequence
 
 n = int(input("Enter the length of the sequence: ")) 
+
+listi= []
+number = 0
+for i in range(1,n+1):
+    if i <= 3:
+        listi.append(i)
+        print(i)
+    elif i > 3:
+        number = listi[-1] + listi[-2] + listi[-3]
+        listi.append(number)
+        print(number)
+
+
+        # If i >= 3 then calculate the sum for last 3 numbers
+    # If i <= 3 then print i and do nothing
