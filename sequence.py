251ab2156bf39ee7fb5d94fb3976f2f8df300e6a
berglindb19 <berglindb19@ru.is>
2020-09-03 14:53:41 +0000

    Algorithm written for sequence

---
+++ sequence.py
@@ -0,0 +1,11 @@
+# Given an lenght of the sequence from the user
+# The sequence constis of the last 3 previous numbers added togeather to genereate
+# The next number in the sequence
+# First number = 1
+# 2nd number = 2
+# 3rd number = 3
+# 4th number = 3 + 2 + 1 
+# 5th number = 6 + 3 + 2 = 11
+# for all n >= 3 the algorithm calculated the sum of the last 3 numbers in the sequence
+
+n = int(input("Enter the length of the sequence: ")) 
