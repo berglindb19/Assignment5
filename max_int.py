309404e12af4f100d145f3d9e085d315d08232ed
berglindb19 <berglindb19@ru.is>
2020-09-03 14:41:46 +0000

    Algorithm written for max_int

--- max_int.py
+++ max_int.py
@@ -1,9 +1,11 @@
 num_int = int(input("Input a number: "))    # Do not change this line
 
-while num_int >= 0:     
-    listi=num_int.split()
-    print(listi)
+listi = []
+
+while num_int >= 0:
+    listi.append(num_int)     
     num_int = int(input("Input a number: "))
 
+print("The maximum is", max(listi))
 
 # print("The maximum is", max_int)  
